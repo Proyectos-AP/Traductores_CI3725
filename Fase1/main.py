7386446ec0eb45dbe498ce1e5195ed3aaff28777
alejandra <alejandra@cc.ac.labf.usb.ve>
2015-09-17 14:42:56 -0430

    Se lee archivo

--- Fase1/main.py
+++ Fase1/main.py
@@ -24,12 +24,99 @@
 #------------------------------------------------------------------------------#
 #						FUNCIONES Y PROCEDIMIENTOS							   #
 #------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+def LeerArchivoEntrada(): 
+  '''
+    Descripción de la función: Lee el archivo de entrada, crea los 
+    paquetes y los almacena en la cola de prioridades.
+    Variables de entrada: Ninguna
+    Variables de salida: Ninguna
+  '''
+
+  # Verificación de la correctitud de los argumentos dados.
+  try:
+  # Se verifica que se introduzcan los argumentos necesarios.
+    assert(len(sys.argv) == 2)
+    NombreArchivoEntrada = sys.argv[1]
+    # Se verifica que el archivo de entrada esté en el directorio
+    # correspondiente.
+    assert(os.path.isfile(NombreArchivoEntrada))
+  except:
+    print("Error: Los argumentos dados no son validos")
+    print("El programa se cerrará.")
+    sys.exit()
+  # Lectura del archivo de entrada.
+  with open(NombreArchivoEntrada,'r') as Archivo:
+    data = Archivo.read()
+    Archivo.close
+
+  return data
 
 #------------------------------------------------------------------------------#
 #						INICIO DEL PROGRAMA PRINCIPAL 						   #
 #------------------------------------------------------------------------------#
 
-print("Hola")
+# List of token names.   This is always required
+tokens = (
+   'NUMBER',
+   'PLUS',
+   'MINUS',
+   'TIMES',
+   'DIVIDE',
+   'LPAREN',
+   'RPAREN',
+)
+
+# Regular expression rules for simple tokens
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+
+# A regular rule with some action code
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)    
+    return t
+
+# Define a rule so we can track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
+
+# Error handling rule
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+    #exit(0)
+
+# Build the lexer
+lexer = lex.lex()
+
+############################################################
+
+datos = LeerArchivoEntrada()
+# Give the lexer some input
+lexer.input(datos)
+
+# Tokenize
+#while True:
+ #   tok = lexer.token()
+  #  if not tok: 
+   #     break      # No more input
+    #print(tok)
+
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok: 
+        break      # No more input
+    print(tok.type, tok.value, tok.lineno, tok.lexpos)
 
 #------------------------------------------------------------------------------#
 #						 FIN DEL PROGRAMA PRINCIPAL 						   #
