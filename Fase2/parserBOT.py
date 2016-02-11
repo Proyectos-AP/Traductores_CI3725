57f9ee17cbd40cb5f354df0be21e90f386c6ac57
Pablo Maldonado <prmm95@gmail.com>
2016-02-11 11:13:32 -0430

    Se eliminaron los comentarios sobrantes de parseBOT.py

--- Fase2/parserBOT.py
+++ Fase2/parserBOT.py
@@ -31,9 +31,7 @@ import ply.yacc as yacc
 #                        DEFINICION DEL MODULO PARSERBOT                       #
 #------------------------------------------------------------------------------#
 
-global Raiz
-# Parsing rules
-
+# Reglas de precedencia a utilizar en las gramaticas:
 precedence = (
     ('left','TkConjuncion','TkDisyuncion'),
     ('left','TkSuma','TkResta'),
@@ -42,12 +40,16 @@ precedence = (
     ('right','UMNEGACION'),
     )
 
+#------------------------------------------------------------------------------#
+
+
 def p_inicioPrograma(t):
     ''' inicio : TkCreate LISTA_DECLARACIONES TkExecute INSTRUCCIONES_CONTROLADOR TkEnd
                 | TkExecute INSTRUCCIONES_CONTROLADOR TkEnd '''
 
-    if(t[1]=="execute"):
+    if (t[1] == "execute"):
         t[0] = RaizAST(None,Execute(t[2]))
+
     elif (t[1] == "create"):
         t[0] = RaizAST(Create(t[2]),Execute(t[4]))
 
@@ -56,9 +58,7 @@ def p_inicioPrograma(t):
 
     return Raiz
 
-
-# def p_IniciolistaDeclaraciones(t):
-#     ''' INICIO_LISTA_DECLARACIONES :  LISTA_DECLARACIONES LISTA_COMPORTAMIENTOS'''
+#------------------------------------------------------------------------------#
 
 def p_listaDeclaraciones(t):
     ''' LISTA_DECLARACIONES : LISTA_DECLARACIONES LISTA_DECLARACIONES
@@ -68,24 +68,23 @@ def p_listaDeclaraciones(t):
 
     if(t[1] in {"int","bool","char"}):
         t[0] = Declaraciones(t[1],t[3],t[4])
+
     else:
         t[0] = agregarHijos(t[1],t[2])
 
+#------------------------------------------------------------------------------#
+
 def  p_listaIdent(t):
     ''' LISTA_IDENT : LISTA_IDENT TkComa LISTA_IDENT'''
-    # t[0]= ListaHijos([t[1]]+[t[2]]
     t[0] = agregarHijos(t[1],t[3])
-    # print(t[1].value)
-    # global Raiz
-    # Raiz = t[0]
  
+#------------------------------------------------------------------------------#
+
 def p_listaIdentUnico(t):
     '''  LISTA_IDENT : TkIdent'''
-    t[0]=Identificadores(t[1])
+    t[0] = Identificadores(t[1])
     
-# def p_IniciolistaComportamientos(t):
-#     ''' INICIO_LISTA_COMPORTAMIENTOS : TkOn CONDICION TkDosPuntos INICIO_INSTRUCCIONES_ROBOT 
-#                               |'''
+#------------------------------------------------------------------------------#
 
 def p_listaComportamientos(t):
     ''' LISTA_COMPORTAMIENTOS : LISTA_COMPORTAMIENTOS LISTA_COMPORTAMIENTOS
@@ -95,10 +94,14 @@ def p_listaComportamientos(t):
     if (len(t) != 1):                     
         if (t[1] == "on"):
             t[0] = ListaComportamiento(t[2],t[4])
+
         else:
             t[0] = agregarHijos(t[1],t[2])
+
     else:
         t[0] = None
+
+#------------------------------------------------------------------------------#
 
 def p_condicion(t):
     ''' CONDICION : TkDeActivation
@@ -107,10 +110,11 @@ def p_condicion(t):
                   | TkDefault''' 
 
     if (t[1] in {"deactivation","activation","default"}):
-        t[0]=Condicion(t[1])
+        t[0] = Condicion(t[1])
     else:
-        t[0]=t[1]
+        t[0] = t[1]
 
+#------------------------------------------------------------------------------#
 
 def p_instruccionRobot(t):
     ''' INSTRUCCIONES_ROBOT : INSTRUCCIONES_ROBOT INSTRUCCIONES_ROBOT
@@ -126,37 +130,29 @@ def p_instruccionRobot(t):
                             | TkSend TkPunto  '''
     if (t[1] == "store"):
         t[0] = Store(t[2])
-        # global Raiz
-        # Raiz = t[0]
+
     elif (t[1] == "recieve"):
         t[0] = Recieve()
-        # global Raiz
-        # Raiz = t[0]
+
     elif (t[1] == "collect"):
         t[0] = Collect(t[2])
-        # global Raiz
-        # Raiz = t[0]
+
     elif (t[1] == "drop"):
         t[0] = Drop(t[2])
-        # global Raiz
-        # Raiz = t[0]
+
     elif (t[1] in {"up","down","left","right"}):
         t[0] = Movimiento(t[1],t[2])
-        # global Raiz
-        # Raiz = t[0]
+
     elif (t[1] == "read"):
         t[0] = Read(t[2])
-        # global Raiz
-        # Raiz = t[0]
+
     elif (t[1] == "send"):
         t[0] = Send()
-        # global Raiz
-        # Raiz = t[0]
+
     else:
         t[0] = agregarHijos(t[1],t[2])
-        # global Raiz
-        # Raiz = t[0]
 
+#------------------------------------------------------------------------------#
  
 def p_expresionOpcional(t):
     ''' EXPRESION_OPCIONAL : EXPRESION_BIN
@@ -166,6 +162,8 @@ def p_expresionOpcional(t):
     else:
         t[0] = None
 
+#------------------------------------------------------------------------------#
+
 def p_guardarVariable(t):
     ''' GUARDAR_VARIABLE : TkAs TkIdent
                         |'''
@@ -174,6 +172,8 @@ def p_guardarVariable(t):
             t[0] = Identificadores(t[2])
     else:
         t[0] = None
+
+#------------------------------------------------------------------------------#
 
 def p_SecuenciaInstruccionesControlador(t):
     '''  INSTRUCCIONES_CONTROLADOR : INSTRUCCIONES_CONTROLADOR INSTRUCCIONES_CONTROLADOR
@@ -188,16 +188,13 @@ def p_SecuenciaInstruccionesControlador(t):
    
     if (t[1]=="activate"):
         t[0] = Activate(t[2])
-        # global Raiz
-        # Raiz = t[0]
+
     elif(t[1]=="advance"):
         t[0] = Advance(t[2])
-        # global Raiz
-        # Raiz = t[0]
+
     elif(t[1]=="deactivate"):
         t[0] = Deactivate(t[2])
-        # global Raiz
-        # Raiz = t[0]
+
     elif (t[1]=="if" and t[5] == "end"):
         t[0] = Condicional(t[2],t[4],None)
 
@@ -205,57 +202,18 @@ def p_SecuenciaInstruccionesControlador(t):
         t[0] = Condicional(t[2],t[4],t[7])
 
     elif(t[1]=="while"):
-
         t[0]= While(t[2],t[4])
 
     elif(t[1]=="execute"):
-
         t[0] = RaizAST(None,Execute(t[2]))
 
     elif (t[1] == "create"):
-
         t[0] = RaizAST(Create(t[2]),Execute(t[4]))
+
     else:
         t[0] = agregarHijos(t[1],t[2])
-    
-# def InstruccionesControlador(t):
 
-#     ''' INSTRUCCIONES_CONTROLADOR : '''
-
-
-# def p_expresionCondicional(t):
-#     ''' EXPRESION_CONDICIONAL : INSTRUCCIONES_CONTROLADOR EXPRESION_ELSE'''
-
-
-# def p_expresionElse(t):
-#     ''' EXPRESION_ELSE : TkElse TkDosPuntos INSTRUCCIONES_CONTROLADOR 
-#                         |'''
-
-# def p_expresionBool(t):
-#     ''' EXPRESION_BOOL : EXPRESION_BOOL TkConjuncion EXPRESION_BOOL
-#                         | EXPRESION_BOOL TkDisyuncion EXPRESION_BOOL
-#                         | EXPRESION_BOOL TkIgual EXPRESION_BOOL
-#                         | EXPRESION_BOOL TkDesigual EXPRESION_BOOL
-#                         | TkNegacion EXPRESION_BOOL
-#                         | EXPRESION_ARITMETICA TkMayor EXPRESION_ARITMETICA
-#                         | EXPRESION_ARITMETICA TkMenor EXPRESION_ARITMETICA
-#                         | EXPRESION_ARITMETICA TkMayorIgual EXPRESION_ARITMETICA
-#                         | EXPRESION_ARITMETICA TkMenorIgual EXPRESION_ARITMETICA
-#                         | TkTrue
-#                         | TkFalse'''
-
-
-# def p_expresionExecute(t):
-#     ''' expressionE : TkExecute expression '''
-
-# def p_statement_assign(t):
-#     'statement : TkIdent TkIgual EXPRESION_BIN'
-#     names[t[1]] = t[3]
-
-# def p_statement_expr(t):
-#     '''statement : EXPRESION_BIN'''
-
-#     #print(t[1])
+#------------------------------------------------------------------------------#
 
 def p_expression_binaria(t):
     '''EXPRESION_BIN : EXPRESION_BIN TkSuma EXPRESION_BIN
@@ -273,90 +231,63 @@ def p_expression_binaria(t):
                   | EXPRESION_BIN TkMenorIgual EXPRESION_BIN'''
 
     t[0] = BinOp(t[1],t[2],t[3])
-    # global Raiz
-    # Raiz=t[0]
-    # print(t[0].op)
-    # print(t[0].left)
-    # print(t[0].right)
-    # if t[2] == '+'  : 
-    #     #t[0] = t[1] + t[3]
-    #     t[0]=str(t[1])+" + "+str(t[3])
-    # elif t[2] == '-': 
-    #     #t[0] = t[1] - t[3]
-    #     t[0] = str(t[1])+" - "+str(t[3])
-    # elif t[2] == '*': 
-    #     #t[0] = t[1] * t[3]
-    #     t[0] = str(t[1])+" * "+str(t[3])
-    # elif t[2] == '/': 
-    #     #t[0] = t[1] / t[3]
-    #     t[0] = str(t[1])+" / "+str(t[3])
-    # elif t[2] == '/\\' : 
-    #     print(str(t[1])+"/\\"+str(t[3]) )
-    # elif t[2] == '\\/': 
-    #     print(str(t[1])+"\\/"+str(t[3]) )
-    #return(Raiz)
 
-# def p_expression_bool(t):
-#     '''expressionBool : expressionBool TkConjuncion expressionBool
-#                       | expressionBool TkDisyuncion expressionBool'''
-#     if t[2] == '/\\'  : print(str(t[1])+"/\\"+str(t[3]) )
-#     elif t[2] == '\\/': print(str(t[1])+"\\/"+str(t[3]) )
-
-# def p_expression_True(t):
-#     '''expressionBool : TkTrue 
-#                         | TkFalse'''
-#     t[0] = t[2]
+#------------------------------------------------------------------------------#
 
 def p_negacion_bool(t):
     '''EXPRESION_BIN : TkNegacion EXPRESION_BIN %prec UMNEGACION'''
     t[0] = OperadorUnario(t[1],t[2])
-    # global Raiz
-    # Raiz=t[0]
 
+#------------------------------------------------------------------------------#
 
 def p_expression_uminus(t):
     'EXPRESION_BIN : TkResta EXPRESION_BIN %prec UMINUS'
     t[0] = OperadorUnario(t[1],t[2])
-    # global Raiz
-    # Raiz=t[0]
+
+#------------------------------------------------------------------------------#
 
 def p_expression_group(t):
     'EXPRESION_BIN : TkParAbre EXPRESION_BIN TkParCierra'
     t[0] = t[2]
 
+#------------------------------------------------------------------------------#
+
 def p_expression_number(t):
     'EXPRESION_BIN : TkNum'
-    # t[0] = t[1]
     t[0] = Number(t[1])
+
+#------------------------------------------------------------------------------#
+
 def p_expression_TrueFalse(t):
     '''EXPRESION_BIN : TkTrue
                      | TkFalse '''
     t[0] = Booleano(t[1])
 
+#------------------------------------------------------------------------------#
+
 def p_expression_name(t):
     'EXPRESION_BIN : TkIdent'
     t[0] = Identificadores(t[1])
 
+#------------------------------------------------------------------------------#
+
 def p_expression_me(t):
     'EXPRESION_BIN : TkMe'
     t[0] = VariableMe(t[1])
-    # try:
-    #     t[0] = names[t[1]]
-    # except LookupError:
-    #     print("Undefined name '%s'" % t[1])
-    #     t[0] = 0
+
+#------------------------------------------------------------------------------#
 
 def p_expression_caracter(t):
     'EXPRESION_BIN : TkCaracter'
     t[0] = Caracter(t[1])
 
+#------------------------------------------------------------------------------#
 
 def p_error(t):
-    if(t!=None):
-        print("Error sintactico '%s' en linea " % t.value,t.lineno)
+    if(t != None):
+        print("Error sintactico " + str(t.value) + " en linea " + str(t.lineno))
     else:
         print("Error sintactico")
-
     sys.exit()
 
-
+#------------------------------------------------------------------------------#
